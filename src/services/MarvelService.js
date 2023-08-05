b7bc41b91b9fd374cd1d88fd94646c7916688b24
ivan <igorlov18@gmail.com>
2023-08-05 19:45:17 +0200

    added MarvelService and RandomChar

---
+++ src/services/MarvelService.js
@@ -0,0 +1,40 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=3aed998a4ecdcfad768c07b80178142e";
+
+  getResources = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = () => {
+    return this.getResources(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+  };
+
+  getCharacter = async (id) => {
+    const res = await this.getResources(
+      `${this._apiBase}/characters/${id}?${this._apiKey}`
+    );
+
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
+  };
+}
+
+export default MarvelService;
